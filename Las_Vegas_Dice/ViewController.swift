ec9de89e561d750b6b2300305599fa17ec49c57f
Suraj_KY <face.surajyadav@gmail.com>
2023-09-27 10:44:29 +0530

    Initial Commit

---
+++ Las_Vegas_Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Las_Vegas_Dice
+//
+//  Created by Suraj KY on 27/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
