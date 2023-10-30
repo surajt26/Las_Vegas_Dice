7f1d9e98d2cb299022a68d24481726235e10ae4c
Suraj_KY <face.surajyadav@gmail.com>
2023-10-30 15:03:39 +0530

    Dice Functionality Completed

--- Las_Vegas_Dice/ViewController.swift
+++ Las_Vegas_Dice/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+    
+    @IBAction func rollButton(_ sender: Any) {
+        let diceArray1 = Int.random(in: 1...6)
+        let diceArray2 = Int.random(in: 1...6)
+        diceImageView1.image = UIImage(named: "dice\(diceArray1)")
+        diceImageView2.image = UIImage(named: "dice\(diceArray2)")
+        print(diceArray1)
+        print(diceArray2)
     }
-
-
 }
 
